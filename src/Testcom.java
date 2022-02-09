b483275b8f2c5b8d76872345ed114cbd56da430a
damirumanets <damirumanets044@gmail.com>
2022-02-09 14:49:57 +0200

    print student

--- src/Testcom.java
+++ src/Testcom.java
@@ -4,12 +4,13 @@ import java.util.List;
 public class Testcom {
     public static void main(String[] args) {
 
-        System.out.println("test commit 7");
+        System.out.println("test commit 8");
         List<String> student=new ArrayList<>();
         student.add("jora");
         student.add("vasya");
         student.add("egor");
         student.remove(0);
+        System.out.println(student);
 
 
 
