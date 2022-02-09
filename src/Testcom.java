635e4406a0d3603bea94a69a72990e06a903bf53
damirumanets <damirumanets044@gmail.com>
2022-02-09 14:54:42 +0200

    remove student1

--- src/Testcom.java
+++ src/Testcom.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class Testcom {
     public static void main(String[] args) {
 
-        System.out.println("test commit 9");
+        System.out.println("test commit 10");
         List<String> student=new ArrayList<>();
         student.add("jora");
         student.add("vasya");
@@ -14,6 +14,8 @@ public class Testcom {
         System.out.println(student);
         List<String> student1=new LinkedList<>();
         student1.add("dima");
+        student1.add("anton");
+        student1.remove(1);
 
 
 
