d5e3312e392964871f3a8701f79fd63e7667ae74
damirumanets <damirumanets044@gmail.com>
2022-02-09 14:53:26 +0200

    create test linkedlist

--- src/Testcom.java
+++ src/Testcom.java
@@ -1,16 +1,21 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Testcom {
     public static void main(String[] args) {
 
-        System.out.println("test commit 8");
+        System.out.println("test commit 9");
         List<String> student=new ArrayList<>();
         student.add("jora");
         student.add("vasya");
         student.add("egor");
         student.remove(0);
         System.out.println(student);
+        List<String> student1=new LinkedList<>();
+        student1.add("dima");
+
+
 
 
 
