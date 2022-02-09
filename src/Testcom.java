11ee2cbbd9d488ca5ae21f237dac80867fd69ea0
damirumanets <damirumanets044@gmail.com>
2022-02-09 14:42:31 +0200

    test commit 4

--- src/Testcom.java
+++ src/Testcom.java
@@ -4,8 +4,10 @@ import java.util.List;
 public class Testcom {
     public static void main(String[] args) {
 
-        System.out.println("test commit 3");
+        System.out.println("test commit 4");
         List<String> student=new ArrayList<>();
+
+
 
     }
 }
